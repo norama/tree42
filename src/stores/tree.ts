15b6c4892c5a74462d3284f673417b0092737490
Nora Mate <norama.matema@gmail.com>
2023-04-09 21:50:10 +0200

    recursive delete

--- src/stores/tree.ts
+++ src/stores/tree.ts
@@ -3,7 +3,7 @@ import { Atom, atom, computed, mapTemplate, keepMount } from 'nanostores'
 export const Node = mapTemplate<TNode, [TData?, string[]?]>((newNode, id, data, childIds) => {
   newNode.setKey('data', data)
   newNode.setKey('childIds', childIds)
-  keepMount(newNode)
+  keepMount(newNode) // otherwise it gets deleted suddenly - why?
   nodeIds.set([...nodeIds.get(), id])
 })
 
@@ -36,5 +36,30 @@ export const deleteChild = (id: string, childId: string) => {
       .get()
       .childIds.filter((cid) => cid !== childId)
   )
-  nodeIds.set(nodeIds.get().filter((cid) => cid !== childId))
+
+  // recursively delete children
+  // may take some time and has no implications to the UI
+  // therefore run with timeout
+  setTimeout(() => {
+    _deleteSubTree(childId)
+  }, 500)
+}
+
+const _deleteSubTree = (rootId: string) => {
+  const deletedIds: string[] = []
+  _collectTree(rootId, deletedIds)
+  console.log('deletedIds', deletedIds)
+  nodeIds.set(nodeIds.get().filter((cid) => !deletedIds.includes(cid)))
+  deletedIds.forEach((cid) => {
+    delete Node.cache[cid]
+  })
+}
+
+const _collectTree = (id: string, treeIds: string[] = []) => {
+  treeIds.push(id)
+  Node(id)
+    .get()
+    .childIds.forEach((childId) => {
+      _collectTree(childId, treeIds)
+    })
 }
